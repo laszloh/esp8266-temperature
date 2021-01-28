416b0d4ad29746278baeab738f1e9b3e2ad26cf7
laszloh <laszlo.hegedues@gmail.com>
2021-01-28 17:47:13 +0100

    Wifi update

--- Software/esp8266-temperature/include/wifi.h
+++ Software/esp8266-temperature/include/wifi.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#define PRIVATE_WIFI_SSID   "***REMOVED***"
-#define PRIVATE_WIFI_PASS   "***REMOVED***"
+#define PRIVATE_WIFI_SSID   ""
+#define PRIVATE_WIFI_PASS   ""
 
 #define PRIVATE_MQTT_LOGIN  ""
 #define PRIVATE_MQTT_PASS   ""
